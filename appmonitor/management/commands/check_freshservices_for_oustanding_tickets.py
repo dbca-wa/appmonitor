d989c04cb6e02e30877caa9d3a21777be9d1d350
Jason Moore <jason.moore@dbca.wa.gov.au>
2023-11-30 17:37:25 +0800

    Fix for multiple groups

--- appmonitor/management/commands/check_freshservices_for_oustanding_tickets.py
+++ appmonitor/management/commands/check_freshservices_for_oustanding_tickets.py
@@ -19,19 +19,23 @@ class Command(BaseCommand):
             auth_request = requests.auth.HTTPBasicAuth(FRESHSERVICES_API_KEY, "X")
             
             for tf in ticket_filters:         
+                tickets_total = 0 
                 tickets = []       
                 try:
                     tickets = requests.get(tf.url,auth=auth_request)
                     tickets_pending = tickets.json()
+                    
                     if "tickets" in tickets_pending:
+                         tickets_total = len(tickets_pending['tickets'])
                          for t in tickets_pending['tickets']:
                               print (t['subject'])                         
                 except Exception as e:
                     print (e)
 
                 t = email_templates.TicketList()
+                t.subject = "Tickets Outstanding for "+str(tf.name) + " Total Tickets ("+str(tickets_total)+")"
                 to_addresses=[]
-                for notification in models.TicketFilterNotification.objects.filter(active=True):
+                for notification in models.TicketFilterNotification.objects.filter(active=True,ticket_filter=tf):
                     print ("Preparing to "+notification.email)
                     to_addresses.append(notification.email)
                 t.send(to_addresses=to_addresses, context={"tickets": tickets_pending, "settings": settings})        
