5f8bf61c2cb7022900256fb1a65932ee23604248
Jason Moore <jason.moore@dbca.wa.gov.au>
2024-07-11 17:52:13 +0800

    Fix Startup

--- startup.sh
+++ startup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Start the first process
-env > /etc/.cronenv
-sed -i 's/\"/\\"/g' /etc/.cronenv
+# env > /etc/.cronenv
+# sed -i 's/\"/\\"/g' /etc/.cronenv
 cat /dev/urandom | tr -dc 'a-f0-9' | fold -w 32 | head -n 1 > /app/git_hash
 
 if [ $ENABLE_CRON == "True" ];
 then
-echo "Starting Cron"
-service cron start &
+echo "Starting Python Cron"
+python scheduler.py ../python-cron /app/logs/python-cron.log &
 status=$?
 if [ $status -ne 0 ]; then
   echo "Failed to start cron: $status"
