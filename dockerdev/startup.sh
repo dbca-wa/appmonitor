b874dc33a2d690fd38987cb6d1702c864d42862e
Jason Moore <jmoore@austwa.com>
2026-09-08 10:05:41 +0800

    Create startup.sh

---
+++ dockerdev/startup.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Start the first process
+# env > /etc/.cronenv
+# sed -i 's/\"/\\"/g' /etc/.cronenv
+ulimit -m 4096000
+ulimit -v 4096000
+# cat /dev/urandom | tr -dc 'a-f0-9' | fold -w 32 | head -n 1 > /app/git_hash
+openssl rand -hex 32 > /app/git_hash
+
+
+if [ $ENABLE_CRON == "True" ];
+then
+echo "Starting Python Cron"
+python /bin/scheduler.py /app/python-cron /app/logs/python-cron.log &
+status=$?
+if [ $status -ne 0 ]; then
+  echo "Failed to start cron: $status"
+  exit $status
+fi
+
+fi
+
+if [ $ENABLE_WEB == "True" ];
+    then
+echo "Starting Gunicorn"
+# Start the second process
+
+/app/venv/bin/gunicorn appmonitor.wsgi --bind :8080 --config /app/gunicorn.ini
+status=$?
+if [ $status -ne 0 ]; then
+  echo "Failed to start gunicorn: $status"
+  exit $status
+fi
+else
+   echo "ENABLE_WEB environment vairable not set to True, web server is not starting."
+   /bin/bash
+fi
+
+if [ $CODE_SERVER_PASSWORD_ENABLED == "True" ];
+    then
+echo "Starting code server"
+# Start the second process
+if [ -n "$CODE_SERVER_PASSWORD" ]; then
+  PASSWORD="$CODE_SERVER_PASSWORD" code-server --bind-addr 0.0.0.0:8443 --auth password /data/data/projects/appmonitor
+fi
+status=$?
+if [ $status -ne 0 ]; then
+  echo "Failed to start gunicorn: $status"
+  exit $status
+fi
+else
+   echo "ENABLE_WEB environment vairable not set to True, web server is not starting."
+   /bin/bash
+fi
