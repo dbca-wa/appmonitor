3e1da90aa8a9fe1b5f7a17690779abcdb0623193
Jason Moore <jmoore@austwa.com>
2026-09-08 10:27:55 +0800

    Update startup.sh

--- dockerdev/startup.sh
+++ dockerdev/startup.sh
@@ -46,10 +46,10 @@ if [ -n "$CODE_SERVER_PASSWORD" ]; then
 fi
 status=$?
 if [ $status -ne 0 ]; then
-  echo "Failed to start gunicorn: $status"
+  echo "Failed to start code server: $status"
   exit $status
 fi
 else
-   echo "ENABLE_WEB environment vairable not set to True, web server is not starting."
+   echo "CODE_SERVER_PASSWORD_ENABLED environment vairable not set to True, code server is not starting."
    /bin/bash
 fi
