0ab55164587923d4b82e0e8da02adfb8d132f16b
Jason Moore <jmoore@austwa.com>
2026-09-08 10:01:32 +0800

    Increase memory limits in startup.sh

--- startup.sh
+++ startup.sh
@@ -3,8 +3,8 @@
 # Start the first process
 # env > /etc/.cronenv
 # sed -i 's/\"/\\"/g' /etc/.cronenv
-ulimit -m 2048000
-ulimit -v 2048000
+ulimit -m 4096000
+ulimit -v 4096000
 # cat /dev/urandom | tr -dc 'a-f0-9' | fold -w 32 | head -n 1 > /app/git_hash
 openssl rand -hex 32 > /app/git_hash
 
